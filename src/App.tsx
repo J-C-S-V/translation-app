248a3d5db358390e152eaa4ed52d98d31e5ff7c9
Juan Sanchez <juansan1993@hotmail.com>
2025-05-14 21:14:14 -0500

    Remove icon file

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Container, Row, Col, Button } from "react-bootstrap";
 import "./App.css";
 import { useStore } from "./hooks/useStore";
 import { AUTO_LANGUAGE } from "./constants";
-import { IconArrow } from "./components/Icons.tsx";
 import { LanguageSelector } from "./components/LanguageSelector";
 import { TextArea } from "./components/TextArea";
 import { useEffect } from "react";
@@ -64,7 +63,15 @@ function App() {
             disabled={fromLanguage === AUTO_LANGUAGE}
             onClick={interchangeLanguages}
           >
-            <IconArrow />
+            <svg
+              width={24}
+              height={24}
+              focusable="false"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <path d="M6.99 11L3 15l3.99 4v-3H14v-2H6.99v-3zM21 9l-3.99-4v3H10v2h7.01v3L21 9z"></path>
+            </svg>
           </Button>
         </Col>
         <Col md={5}>
